fb82590e7a1eccf69a5f62f079adee7d46a315fe
LuckyE1303 <peggy880311@gmail.com>
2019-08-30 00:02:27 +0800

    add laser to pointcloud

--- 04-trail-following-robot/catkin_ws/src/obstacle_detection/src/obs_detect.cpp
+++ 04-trail-following-robot/catkin_ws/src/obstacle_detection/src/obs_detect.cpp
@@ -1,12 +1,15 @@
 #include <ros/ros.h>
 #include <pcl_ros/point_cloud.h>
+#include <pcl/point_cloud.h>
 #include <pcl/point_types.h>
-// #include <pcl_conversions.h>
-#include <boost/foreach.hpp>
 #include <laser_geometry/laser_geometry.h>
+#include <tf/transform_listener.h>
+#include <sensor_msgs/PointCloud2.h>
 
-laser_geometry::LaserProjection projector_
-ensor_msgs::PointCloud2 oint_cloud_publisher_;
+
+
+laser_geometry::LaserProjection projector_;
+ros::Publisher point_cloud_publisher_;
 tf::TransformListener listener_;
 
 typedef pcl::PointCloud<pcl::PointXYZ> PointCloud;
@@ -24,16 +27,15 @@ void laserscan_cb(const sensor_msgs::LaserScan::ConstPtr& scan_in)
     }
 
     sensor_msgs::PointCloud2 cloud;
-    projector_.transformLaserScanToPointCloud("base_link", *scan, cloud, tfListener_);
+    projector_.transformLaserScanToPointCloud("base_link", *scan_in, cloud, listener_);
     point_cloud_publisher_.publish(cloud);
-    
 }
 
 int main(int argc, char** argv)
 {
     ros::init(argc, argv, "sub_pcl");
     ros::NodeHandle nh;
-    ros::Subscriber sub = nh.subscribe<PointCloud>("/scan", 1, laserscan_cb);
-    point_cloud_publisher_ = nh.advertise<sensor_msgs::PointCloud2> ("/cloud", 100);
+    ros::Subscriber sub = nh.subscribe<sensor_msgs::LaserScan>("/scan", 1, laserscan_cb);
+    point_cloud_publisher_ = nh.advertise<sensor_msgs::PointCloud2> ("/cloud", 10);
     ros::spin();
 }
