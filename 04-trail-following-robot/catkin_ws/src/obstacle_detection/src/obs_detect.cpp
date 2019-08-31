1a50b66086fec6918ce10aa1463bbfd845d0d975
coolcat647 <b10302017@mail.ntust.edu.tw>
2019-08-31 08:46:00 +0800

    fix bug

--- 04-trail-following-robot/catkin_ws/src/obstacle_detection/src/obs_detect.cpp
+++ 04-trail-following-robot/catkin_ws/src/obstacle_detection/src/obs_detect.cpp
@@ -1,39 +1,104 @@
+#include <string.h>
+#include <iostream>
 #include <ros/ros.h>
+#include <ros/console.h>
+#include <sensor_msgs/PointCloud2.h>
+#include <sensor_msgs/LaserScan.h>
+#include <sensor_msgs/Range.h>
 #include <pcl_ros/point_cloud.h>
 #include <pcl/point_cloud.h>
 #include <pcl/point_types.h>
+#include <pcl/filters/conditional_removal.h>
+#include <pcl_conversions/pcl_conversions.h>
+#include <pcl_ros/transforms.h> 
 #include <laser_geometry/laser_geometry.h>
 #include <tf/transform_listener.h>
-#include <sensor_msgs/PointCloud2.h>
+#include <tf/message_filter.h>
+#include <message_filters/subscriber.h>
 
-laser_geometry::LaserProjection projector_;
-ros::Publisher point_cloud_publisher_;
-tf::TransformListener listener_;
 
-typedef pcl::PointCloud<pcl::PointXYZ> PointCloud;
+using namespace std;
 
-void laserscan_cb(const sensor_msgs::LaserScan::ConstPtr& scan_in)
-{
-    // printf ("Cloud: width = %d, height = %d\n", msg->width, msg->height);
-    // BOOST_FOREACH (const pcl::PointXYZ& pt, msg->points)
-    // printf ("\t(%f, %f, %f)\n", pt.x, pt.y, pt.z);
-    if(!listener_.waitForTransform(scan_in->header.frame_id,
-        "/base_link",
-        scan_in->header.stamp + ros::Duration().fromSec(scan_in->ranges.size()*scan_in->time_increment),
-        ros::Duration(1.0))){
-        return;
+typedef pcl::PointCloud<pcl::PointXYZ> PointCloudXYZ;
+
+class LaserObstacleDetection{
+    public:
+    ros::NodeHandle nh_;
+    laser_geometry::LaserProjection projector_;
+    ros::Publisher pub_cloud_;
+    ros::Publisher pub_range_;
+    tf::TransformListener listener_;
+    ros::Subscriber sub_laser_;
+    sensor_msgs::Range danger_r;
+
+    LaserObstacleDetection(ros::NodeHandle n): nh_(n){
+        // ROS subscriber
+        sub_laser_ = nh_.subscribe<sensor_msgs::LaserScan>("scan", 1, &LaserObstacleDetection::laserscan_cb, this);
+        // ROS publisher
+        pub_cloud_ = nh_.advertise<sensor_msgs::PointCloud2> ("cloud", 1);
+        pub_range_ = nh_.advertise<sensor_msgs::Range> ("danger_field", 1);
+        
+        danger_r.header.frame_id = "laser_frame";
+        danger_r.radiation_type = sensor_msgs::Range::INFRARED;
+        danger_r.field_of_view = 3.1415926;
+        danger_r.min_range = 0.1;
+        danger_r.max_range = 1.5;
+        
     }
 
-    sensor_msgs::PointCloud2 cloud;
-    projector_.transformLaserScanToPointCloud("base_link", *scan_in, cloud, listener_);
-    point_cloud_publisher_.publish(cloud);
-}
+    void laserscan_cb(const sensor_msgs::LaserScan::ConstPtr& scan_in){
+        sensor_msgs::PointCloud2 cloud_in;
+        PointCloudXYZ::Ptr cloud(new PointCloudXYZ);
+        PointCloudXYZ::Ptr cloud_filtered(new PointCloudXYZ);
+        // projector_.transformLaserScanToPointCloud("base_link", *scan_in, cloud_in, listener_);
+        projector_.projectLaser (*scan_in, cloud_in);
+        pcl::fromROSMsg(cloud_in, *cloud);
+
+        pcl::ConditionAnd<pcl::PointXYZ>::Ptr range_cond (new pcl::ConditionAnd<pcl::PointXYZ> ());
+        range_cond->addComparison (pcl::FieldComparison<pcl::PointXYZ>::ConstPtr (new
+            pcl::FieldComparison<pcl::PointXYZ> ("x", pcl::ComparisonOps::GT, -1.5)));
+        range_cond->addComparison (pcl::FieldComparison<pcl::PointXYZ>::ConstPtr (new
+            pcl::FieldComparison<pcl::PointXYZ> ("x", pcl::ComparisonOps::LT, 0.0)));
+        range_cond->addComparison (pcl::FieldComparison<pcl::PointXYZ>::ConstPtr (new
+            pcl::FieldComparison<pcl::PointXYZ> ("y", pcl::ComparisonOps::GT, -0.8)));
+        range_cond->addComparison (pcl::FieldComparison<pcl::PointXYZ>::ConstPtr (new
+            pcl::FieldComparison<pcl::PointXYZ> ("y", pcl::ComparisonOps::LT, 0.8)));
+        // build the filter
+        pcl::ConditionalRemoval<pcl::PointXYZ> condrem;
+        condrem.setCondition (range_cond);
+        condrem.setInputCloud (cloud);
+        condrem.setKeepOrganized(true);
+        // apply filter
+        condrem.filter (*cloud_filtered);
+            
+        tf::TransformListener listener;
+        tf::StampedTransform tf1;
+        try{
+            listener.waitForTransform("base_link", "laser_frame", ros::Time(0), ros::Duration(10.0) );
+            listener.lookupTransform("base_link", "laser_frame", ros::Time(0), tf1);
+        }
+        catch (tf::TransformException ex){
+            ROS_ERROR("%s",ex.what());
+            ros::Duration(1.0).sleep();
+            return;
+        }
+        
+        pcl_ros::transformPointCloud (*cloud_filtered, *cloud_filtered, tf1);
+        
+        // Publish the data
+        pub_cloud_.publish(*cloud_filtered);
+        danger_r.header.stamp = ros::Time::now();
+        danger_r.range = 1;
+        pub_range_.publish(danger_r);
+    }
+};
+
 
 int main(int argc, char** argv)
 {
     ros::init(argc, argv, "sub_pcl");
     ros::NodeHandle nh;
-    ros::Subscriber sub = nh.subscribe<sensor_msgs::LaserScan>("/scan", 1, laserscan_cb);
-    point_cloud_publisher_ = nh.advertise<sensor_msgs::PointCloud2> ("/cloud", 10);
+    LaserObstacleDetection obj(nh);
     ros::spin();
+    return 0;
 }
