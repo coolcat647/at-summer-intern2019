1ad3ba904e29a81f96b2c7fadc7637ae6c431e7e
coolcat647 <b10302017@mail.ntust.edu.tw>
2019-09-02 15:13:41 +0800

    modify obs_detect code

--- 04-trail-following-robot/catkin_ws/src/obstacle_detection/src/obs_detect.cpp
+++ 04-trail-following-robot/catkin_ws/src/obstacle_detection/src/obs_detect.cpp
@@ -1,5 +1,6 @@
 #include <string.h>
 #include <iostream>
+#include <math.h>
 #include <ros/ros.h>
 #include <ros/console.h>
 #include <sensor_msgs/PointCloud2.h>
@@ -16,6 +17,12 @@
 #include <tf/message_filter.h>
 #include <message_filters/subscriber.h>
 
+#define PI 3.14159265f
+#define DEGREE_OF_VIEW 90.0f
+#define DEGREE_OF_CENTER 40.0f
+#define DANGER_DISTANCE 1
+#define UNSAFETY_DISTANCE 1.5
+
 
 using namespace std;
 
@@ -24,36 +31,85 @@ typedef pcl::PointCloud<pcl::PointXYZ> PointCloudXYZ;
 class LaserObstacleDetection{
     public:
     ros::NodeHandle nh_;
-    laser_geometry::LaserProjection projector_;
+    
     ros::Publisher pub_cloud_;
     ros::Publisher pub_range_;
-    tf::TransformListener listener_;
     ros::Subscriber sub_laser_;
-    sensor_msgs::Range danger_r;
+    laser_geometry::LaserProjection projector_;
+    tf::TransformListener listener_;
+    sensor_msgs::Range detection_range;
+
+    enum FILED{
+        DANGER_FRONT= 0,
+        DANGER_LEFT,
+        DANGER_RIGHT,
+        UNSAFETY_FRONT,
+        UNSAFETY_LEFT,
+        UNSAFETY_RIGHT,
+        DONT_CARE,
+    };
+
+    const std::string FILED_NAMES[7] = {"DANGER_FRONT",
+        "DANGER_LEFT",
+        "DANGER_RIGHT",
+        "UNSAFETY_FRONT",
+        "UNSAFETY_LEFT",
+        "UNSAFETY_RIGHT",
+        "DONT_CARE"
+    };
+    
 
     LaserObstacleDetection(ros::NodeHandle n): nh_(n){
         // ROS subscriber
         sub_laser_ = nh_.subscribe<sensor_msgs::LaserScan>("scan", 1, &LaserObstacleDetection::laserscan_cb, this);
         // ROS publisher
         pub_cloud_ = nh_.advertise<sensor_msgs::PointCloud2> ("cloud", 1);
-        pub_range_ = nh_.advertise<sensor_msgs::Range> ("danger_field", 1);
-        
-        danger_r.header.frame_id = "laser_frame";
-        danger_r.radiation_type = sensor_msgs::Range::INFRARED;
-        danger_r.field_of_view = 3.1415926;
-        danger_r.min_range = 0.1;
-        danger_r.max_range = 1.5;
-        
+        pub_range_ = nh_.advertise<sensor_msgs::Range> ("detection_field", 1);
+        detection_range.header.frame_id = "laser_frame";
+        detection_range.radiation_type = sensor_msgs::Range::INFRARED;
+        detection_range.field_of_view = DEGREE_OF_VIEW * PI / 180.0;
+        detection_range.min_range = 0.1;
+        detection_range.max_range = UNSAFETY_DISTANCE;
+        detection_range.range = UNSAFETY_DISTANCE;
+
+    }
+
+    uint16_t point_to_field(float x, float y){
+        float d = sqrt(y*y + x*x);
+        float angle = atan2(y, x) * 180 / PI;
+        FILED label;
+        if(d < DANGER_DISTANCE){
+            if(abs(angle) <= DEGREE_OF_CENTER/2)
+                label = DANGER_FRONT; 
+            else if(angle > DEGREE_OF_CENTER/2 && angle < DEGREE_OF_VIEW/2)
+                label = DANGER_LEFT;
+            else if(angle < -DEGREE_OF_CENTER/2 && angle > -DEGREE_OF_VIEW/2)
+                label = DANGER_RIGHT;          
+        }
+        else if(d >= DANGER_DISTANCE && d < UNSAFETY_DISTANCE){
+            if(abs(angle) <= DEGREE_OF_CENTER/2)
+                label = UNSAFETY_FRONT; 
+            else if(angle > DEGREE_OF_CENTER/2 && angle < DEGREE_OF_VIEW/2)
+                label = UNSAFETY_LEFT;
+            else if(angle < -DEGREE_OF_CENTER/2 && angle > -DEGREE_OF_VIEW/2)
+                label = UNSAFETY_RIGHT;
+        }
+        else
+            label = DONT_CARE;
+
+        return label;
     }
 
     void laserscan_cb(const sensor_msgs::LaserScan::ConstPtr& scan_in){
         sensor_msgs::PointCloud2 cloud_in;
         PointCloudXYZ::Ptr cloud(new PointCloudXYZ);
         PointCloudXYZ::Ptr cloud_filtered(new PointCloudXYZ);
-        // projector_.transformLaserScanToPointCloud("base_link", *scan_in, cloud_in, listener_);
+        
+        // Convert laser scan data to pointcloud
         projector_.projectLaser (*scan_in, cloud_in);
         pcl::fromROSMsg(cloud_in, *cloud);
 
+        // Filter point cloud by specific 
         pcl::ConditionAnd<pcl::PointXYZ>::Ptr range_cond (new pcl::ConditionAnd<pcl::PointXYZ> ());
         range_cond->addComparison (pcl::FieldComparison<pcl::PointXYZ>::ConstPtr (new
             pcl::FieldComparison<pcl::PointXYZ> ("x", pcl::ComparisonOps::GT, -1.5)));
@@ -63,14 +119,17 @@ class LaserObstacleDetection{
             pcl::FieldComparison<pcl::PointXYZ> ("y", pcl::ComparisonOps::GT, -0.8)));
         range_cond->addComparison (pcl::FieldComparison<pcl::PointXYZ>::ConstPtr (new
             pcl::FieldComparison<pcl::PointXYZ> ("y", pcl::ComparisonOps::LT, 0.8)));
-        // build the filter
+        // Build the filter
         pcl::ConditionalRemoval<pcl::PointXYZ> condrem;
         condrem.setCondition (range_cond);
         condrem.setInputCloud (cloud);
         condrem.setKeepOrganized(true);
-        // apply filter
+        // Apply filter
         condrem.filter (*cloud_filtered);
-            
+        
+
+        
+        // Transform pointcloud to base_link view
         tf::TransformListener listener;
         tf::StampedTransform tf1;
         try{
@@ -84,12 +143,25 @@ class LaserObstacleDetection{
         }
         
         pcl_ros::transformPointCloud (*cloud_filtered, *cloud_filtered, tf1);
+
+
+        uint16_t vote[7] = {0};
+        for (int i = 0; i < cloud_filtered->points.size(); i++){
+            int idx = point_to_field(cloud_filtered->points[i].x, cloud_filtered->points[i].y);
+            vote[idx]++;
+        }
+        for(int i=0; i < 7; i++){
+            if(i==3) cout << "\n";
+            cout << "[" << FILED_NAMES[i] << "]:" << vote[i] << ",\t";
+        }
+        cout << "\n======================" << endl;
         
+
         // Publish the data
         pub_cloud_.publish(*cloud_filtered);
-        danger_r.header.stamp = ros::Time::now();
-        danger_r.range = 1;
-        pub_range_.publish(danger_r);
+
+        detection_range.header.stamp = ros::Time::now();
+        pub_range_.publish(detection_range);
     }
 };
 
